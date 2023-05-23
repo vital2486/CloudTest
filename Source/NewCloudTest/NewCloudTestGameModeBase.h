247a8135ff97658eab60b7cae6ededaed8f32169
CookK_S <vital2486@naver.com>
2023-05-23 13:55:31 +0900

    Init Project

---
+++ Source/NewCloudTest/NewCloudTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NewCloudTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWCLOUDTEST_API ANewCloudTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
